95d780a74ffea2ddc651d35739b1f20f6d773896
MrWhiteres <alexandr.leluk.99@gmail.com>
2021-12-04 20:54:28 +0200

    add prod setings

--- deploy.py
+++ deploy.py
@@ -34,7 +34,7 @@ def find_instances():
         {'Name': 'instance-state-name', 'Values': ['running']},
         {'Name': 'tag:Role', 'Values': ['movies-app']},
     ])
-    return running_instances, [print("Id: {0}\nPlatform: {1}\nType: {2}\nPublic IPv4: {3}\nAMI: {4}\nState: {5}\n".format(
+    return [print("Id: {0}\nPlatform: {1}\nType: {2}\nPublic IPv4: {3}\nAMI: {4}\nState: {5}\n".format(
          instance.id, instance.platform, instance.instance_type, instance.public_ip_address, instance.image.id, instance.state
          )) for instance in ec2.instances.all()]
 
